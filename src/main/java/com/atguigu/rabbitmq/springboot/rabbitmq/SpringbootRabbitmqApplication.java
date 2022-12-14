7534b6989af764c2d2ba7bbc46bf12d347052756
sunkai <sunkai2@founder.com.cn>
2022-12-14 18:11:51 +0800

    rabbitmq延迟队列

--- src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/SpringbootRabbitmqApplication.java
+++ src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/SpringbootRabbitmqApplication.java
@@ -3,6 +3,9 @@ package com.atguigu.rabbitmq.springboot.rabbitmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author sunkai
+ */
 @SpringBootApplication
 public class SpringbootRabbitmqApplication {
 
---
+++ src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/conf/TtlQueueConfig.java
@@ -0,0 +1,100 @@
+package com.atguigu.rabbitmq.springboot.rabbitmq.conf;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sunkai
+ */
+@Configuration
+public class TtlQueueConfig {
+    //普通交换机名称
+    public static final String X_EXCHANGE = "X";
+    //死信交换机名称
+    public static final String Y_DEAD_LETTER_EXCHANGE = "Y";
+    //普通队列名称
+    public static final String QUEUE_A = "QA";
+    public static final String QUEUE_B = "QB";
+    public static final String QUEUE_C = "QC";
+    //死信队列名称
+    public static final String DEAD_LETTER_QUEUE = "QD";
+
+    // 声明 xExchange
+    @Bean("xExchange")
+    public DirectExchange xExchange() {
+        return new DirectExchange(X_EXCHANGE);
+    }
+
+    // 声明 yExchange
+    @Bean("yExchange")
+    public DirectExchange yExchange() {
+        return new DirectExchange(Y_DEAD_LETTER_EXCHANGE);
+    }
+
+    @Bean("queueA")
+    public Queue queueA() {
+        Map<String, Object> args = new HashMap<>(3);
+        //声明当前队列绑定的死信交换机
+        args.put("x-dead-letter-exchange", Y_DEAD_LETTER_EXCHANGE);
+        //声明当前队列的死信路由 key
+        args.put("x-dead-letter-routing-key", "YD");
+        //声明队列的 TTL
+        args.put("x-message-ttl", 10000);
+        return QueueBuilder.durable(QUEUE_A).withArguments(args).build();
+    }
+    // 声明队列 A 绑定 X 交换机
+    @Bean
+    public Binding queueaBindingX(@Qualifier("queueA") Queue queueA,
+                                  @Qualifier("xExchange") DirectExchange xExchange){
+        return BindingBuilder.bind(queueA).to(xExchange).with("XA");
+    }
+    //声明队列 B ttl 为 40s 并绑定到对应的死信交换机
+    @Bean("queueB")
+    public Queue queueB(){
+        Map<String, Object> args = new HashMap<>(3);
+        //声明当前队列绑定的死信交换机
+        args.put("x-dead-letter-exchange", Y_DEAD_LETTER_EXCHANGE);
+        //声明当前队列的死信路由 key
+        args.put("x-dead-letter-routing-key", "YD");
+        //声明队列的 TTL
+        args.put("x-message-ttl", 40000);
+        return QueueBuilder.durable(QUEUE_B).withArguments(args).build();
+    }
+    //声明队列 B 绑定 X 交换机
+    @Bean
+    public Binding queuebBindingX(@Qualifier("queueB") Queue queue1B,
+                                  @Qualifier("xExchange") DirectExchange xExchange){
+        return BindingBuilder.bind(queue1B).to(xExchange).with("XB");
+    }
+    @Bean("queueC")
+    public Queue queueC(){
+        Map<String, Object> args = new HashMap<>(3);
+        //声明当前队列绑定的死信交换机
+        args.put("x-dead-letter-exchange", Y_DEAD_LETTER_EXCHANGE);
+        //声明当前队列的死信路由 key
+        args.put("x-dead-letter-routing-key", "YD");
+        return QueueBuilder.durable(QUEUE_C).withArguments(args).build();
+    }
+    //声明队列 C 绑定 X 交换机
+    @Bean
+    public Binding queuecBindingX(@Qualifier("queueC") Queue queue1C,
+                                  @Qualifier("xExchange") DirectExchange xExchange){
+        return BindingBuilder.bind(queue1C).to(xExchange).with("XC");
+    }
+    //声明死信队列 QD
+    @Bean("queueD")
+    public Queue queueD(){
+        return new Queue(DEAD_LETTER_QUEUE);
+    }
+    //声明死信队列 QD 绑定关系
+    @Bean
+    public Binding deadLetterBindingQAD(@Qualifier("queueD") Queue queueD,
+                                        @Qualifier("yExchange") DirectExchange yExchange){
+        return BindingBuilder.bind(queueD).to(yExchange).with("YD");
+    }
+}
