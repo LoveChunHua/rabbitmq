14bf46e6c944ed48767cfe5e0dbfba721c76afb6
sunkai <18765861848@163.com>
2022-12-14 23:23:33 +0800

    发布确认模式、备份交换机、优先级队列、

--- src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/conf/DelayedQueueConfig.java
+++ src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/conf/DelayedQueueConfig.java
@@ -17,23 +17,23 @@ public class DelayedQueueConfig {
     public static final String DELAYED_EXCHANGE_NAME = "delayed.exchange";
     public static final String DELAYED_ROUTING_KEY = "delayed.routingkey";
 
-    @Bean
-    public Queue delayedQueue() {
-        return new Queue(DELAYED_QUEUE_NAME);
-    }
+//    @Bean
+//    public Queue delayedQueue() {
+//        return new Queue(DELAYED_QUEUE_NAME);
+//    }
 
     //自定义交换机 我们在这里定义的是一个延迟交换机
-    @Bean
-    public CustomExchange delayedExchange() {
-        Map<String, Object> args = new HashMap<>();
-        //自定义交换机的类型
-        args.put("x-delayed-type", "direct");
-        return new CustomExchange(DELAYED_EXCHANGE_NAME, "x-delayed-message", true, false, args);
-    }
+//    @Bean
+//    public CustomExchange delayedExchange() {
+//        Map<String, Object> args = new HashMap<>();
+//        //自定义交换机的类型
+//        args.put("x-delayed-type", "direct");
+//        return new CustomExchange(DELAYED_EXCHANGE_NAME, "x-delayed-message", true, false, args);
+//    }
 
-    @Bean
-    public Binding bindingDelayedQueue(@Qualifier("delayedQueue") Queue queue,
-                                       @Qualifier("delayedExchange") CustomExchange delayedExchange) {
-        return BindingBuilder.bind(queue).to(delayedExchange).with(DELAYED_ROUTING_KEY).noargs();
-    }
+//    @Bean
+//    public Binding bindingDelayedQueue(@Qualifier("delayedQueue") Queue queue,
+//                                       @Qualifier("delayedExchange") CustomExchange delayedExchange) {
+//        return BindingBuilder.bind(queue).to(delayedExchange).with(DELAYED_ROUTING_KEY).noargs();
+//    }
 }
--- src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/consumer/DelayQueueConsumer.java
+++ src/main/java/com/atguigu/rabbitmq/springboot/rabbitmq/consumer/DelayQueueConsumer.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class DelayQueueConsumer {
     public static final String DELAYED_QUEUE_NAME = "delayed.queue";
 
-    @RabbitListener(queues = DELAYED_QUEUE_NAME)
+//    @RabbitListener(queues = DELAYED_QUEUE_NAME)
     public void receiveDelayedQueue(Message message) {
         String msg = new String(message.getBody());
         log.info("当前时间：{},收到延时队列的消息 ：{}", new Date().toString(), msg);
